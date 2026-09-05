1b9f5e02978e9e978ebd50da6160671c86ea7057
Zea <zeaintelligence@gmail.com>
2026-09-05 17:59:04 +0700

    Add files via upload

---
+++ env.js
@@ -0,0 +1,52 @@
+
+
+
+DEEPSEEK_API_KEY=
+
+
+# ==========================================
+# PENGATURAN DATABASE POSTGRESQL
+# ==========================================
+
+DB_USER=
+
+DB_HOST=
+
+DB_NAME=
+
+DB_PASSWORD=
+
+DB_PORT=
+
+
+# ==========================================
+# PENGATURAN WEB & DASHBOARD
+# =========================================
+
+PORT=
+
+SESSION_SECRET=
+
+IP_WHITELIST=
+
+JWT_SECRET=
+
+DASHBOARD_PW=
+
+HISTORY_FILE=
+
+TUNNEL_NAME=
+
+######$$##############
+
+META_ACCESS_TOKEN=
+
+META_PHONE_NUMBER_ID=
+
+META_VERIFY_TOKEN=
+
+META_VERSION=
+
+
+
+
